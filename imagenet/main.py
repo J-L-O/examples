9cfe009408e68dab58daf7ee4f0708695006ad5d
Jona Otholt <18723283+J-L-O@users.noreply.github.com>
2020-06-11 21:12:12 +0200

    Bugfix

--- imagenet/main.py
+++ imagenet/main.py
@@ -363,10 +363,10 @@ def plot_classes_preds(net, images, labels):
         ax = fig.add_subplot(1, 4, idx+1, xticks=[], yticks=[])
         matplotlib_imshow(images[idx], one_channel=True)
         ax.set_title("{0}, {1:.1f}%\n(label: {2})".format(
-            classes[preds[idx]],
+            preds[idx],
             probs[idx] * 100.0,
-            classes[labels[idx]]),
-                    color=("green" if preds[idx]==labels[idx].item() else "red"))
+            labels[idx]),
+                    color=("green" if preds[idx] == labels[idx].item() else "red"))
     return fig
 
 
